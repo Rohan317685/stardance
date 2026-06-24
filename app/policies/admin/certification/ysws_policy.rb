aa12eccffe34f9daa339581491f9bb1b235ae9e7
Alex Van Doren <73424106+vandorena@users.noreply.github.com>
2026-06-24 15:45:25 -0400

    Update ysws_policy.rb

--- app/policies/admin/certification/ysws_policy.rb
+++ app/policies/admin/certification/ysws_policy.rb
@@ -1,6 +1,5 @@
 class Admin::Certification::YswsPolicy < ApplicationPolicy
   def index?
-    return true if user.nil? # temp bypass for dev
     user.admin? || user.has_role?(:guardian_of_integrity)
   end
 
